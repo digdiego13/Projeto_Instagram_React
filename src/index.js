a475a44974dee2965a8bf4b997aebaf7a5c2a5a0
Diego Mamede Nogueira <diegomamede@poli.ufrj.br>
2021-08-22 22:43:24 -0300

    Topo feito

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import ReactDOM  from "react-dom";
+import Topo from "./topo";
+
+function App() {
+
+    return (
+        <Topo />
+
+    )
+}
+const app = App();
+ReactDOM.render(app, document.querySelector(".root"));
